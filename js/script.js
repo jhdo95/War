3ef4df8dcf58c9fb539f08bce399fec50cefb5fb
jhdo95 <jhdo95@gmail.com>
2023-08-08 12:37:04 -0700

    Added State Variables as well as init/renderResults function

--- js/script.js
+++ js/script.js
@@ -5,8 +5,13 @@ const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', '
 const originalDeck = buildOriginalDeck();
 renderDeckInContainer(originalDeck, document.querySelectorAll('card'));
 
-/*----- state variables -----*/
-let shuffledDeck;
+/*----- state variables (things to be remembered throughout execution of program can think of like global data.) -----*/ 
+let shuffledDeck; //
+let results; // Object key of 'player1', 'computer'
+             // Checks whether player1's or computers card is higher. Tie = war.
+             
+let winner; // String of 'p' if player wins, 'c' if computer wins.
+
 
 /*----- cached elements  -----*/
 const shuffledContainer =
@@ -16,12 +21,22 @@ document.getElementById('shuffled-deck-container');
 
 
 /*----- functions -----*/
+init();
+// Initialize all state, then call render();
+function init() {
+    results = {
+        p: '',
+        c: '',
+    };
+    winner = null;
+    render();
+}
+
 function renderNewShuffledDeck() {
     //Create copy of OG Deck (leeave og untouched)
     shuffledDeck = getNewShuffledDeck();
     renderDeckInContainer(shuffledDeck, shuffledContainer);
 }
-
 function renderDeckInContainer(deck, container) {
     container.innerHTML = '';
     //Build cards as a string of HTML
@@ -31,7 +46,6 @@ function renderDeckInContainer(deck, container) {
     });
     container.innerHTML = cardsHtml;
 }
-
 function getNewShuffledDeck() {
     //Create a copy of the OriginalDeck (leave originalDeck untouched!)
     const tempDeck = [...originalDeck];
@@ -45,7 +59,6 @@ function getNewShuffledDeck() {
     return newShuffledDeck;
 }
 
-//function renderDeckInContainer
 
 function buildOriginalDeck() {
     const deck = [];
@@ -61,7 +74,17 @@ function buildOriginalDeck() {
     });
     return deck;
 }
+function renderResults(); {
 
+    
+}
+
+function render() {
+   // renderNewShuffledDeck();
+    renderResults();
+
+
+}
 
 
 //Initialize the deck and shuffle it
