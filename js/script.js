62a69e3eb210f49a69a8f2dd39bbddec1e847d8c
jhdo95 <jhdo95@gmail.com>
2023-08-10 13:48:10 -0700

    save for adding to deploy game

--- js/script.js
+++ js/script.js
@@ -29,8 +29,6 @@ const splitPoint = Math.floor(shuffledDeck.length / 2);
 const playerCards = shuffledDeck.slice(0, splitPoint);
 const computerCards = shuffledDeck.slice(splitPoint);
 
-//cCardEl.classList.add('dA');
-
 /*----- event listeners -----*/
 drawButton.addEventListener('click', () => {
     playRound();
@@ -49,50 +47,74 @@ function playRound() {
         //Player wins and will add the cards to the player's deck.
         playerCards.unshift(playerCard, computerCard);
         //update results 
-        results.p = 'win';
+        results.p = 'p';
     } else if (computerCard.value > playerCard.value) {
         //computer wins and can add the cards to the computer's deck
         computerCards.unshift(playerCard, computerCard);
         //update results
-        results.c = 'win';
+        results.c = 'c';
     } else {
         // It's a tie, time to go to war!
         initiateWar(playerCard, computerCard);
     }
-    // const playerCardImagePath = `css/card-library/images/${playerCard.face}.png`;
-    // const computerCardImagePath = `css/card-library/images/${computerCard.face}.png`;
 
-    // console.log('Player Card Image Path:', playerCardImagePath);
-    // console.log('Computer Card Image Path:', computerCardImagePath);
-
-    // pCardEl.style.backgroundImage = `url(${playerCardImagePath})`;
-    // cCardEl.style.backgroundImage = `url(${computerCardImagePath})`;
 
    
-    console.log('playerCard', playerCard)
+    console.log('playerCard:', playerCard);
+    console.log('computerCard:',computerCard);
+    console.log('playerCards:', playerCards);
+    console.log('computerCards:', computerCards);
     //Update card display
     renderDeckInContainer(playerCard, pCardEl);
     renderDeckInContainer(computerCard, cCardEl);
     renderResults();
+    
     }
 }
 
 function initiateWar(playerCard, computerCard) {
-    //Place additional cards on the "field" and compare the final cards
-    //Update results
+    const warCards = [playerCard, computerCard]; //Start with cards that caused the war
+    // Each player places 4 cards down
+    for (let i = 0; i < 4; i++) {
+        // Check if any player has run out of cards
+        if (playerCards.length === 0) {
+            results.c = 'c'; // Computer wins as player is out of cards.
+            return;
+        } else if (computerCards.length === 0) {
+            results.p = 'p';
+            return;
+        }
+        // Flip a card from each player's deck
+        const playerWarCard = playerCards.pop();
+        const computerWarCard = computerCards.pop();
+
+        // Add the war cards to the array
+        warCards.push(playerWarCard, computerWarCard);
+    }
+    // Compare the final cards after war
+    const playerWarValue = warCards[warCards.length - 1].value;
+    const computerWarValue = warCards[warCards.length - 2].value;
+
+    if (playerWarValue > computerWarValue) {
+        // Player wins the war and takes all the war cards.
+        playerCards.unshift(...warCards);
+        results.p = 'p';
+    } else if (computerWarValue > playerWarValue) {
+        // Computer wins the war and takes all the cards.
+        computerCards.unshift(...warCards);
+        results.c = 'c';
+    } else {
+        // Another war as the war cards are of the same rank.
+        const newPlayerWar = playerCards.pop();
+        const newComputerWar = computerCards.pop();
+        initiateWar(newPlayerWar, newComputerWar);
+    }
+    render();
 }
 
 function renderDeckInContainer(card, container) {
     container.innerHTML = '';
-    // Let's build the cards as a string of HTML
-    // container.classList.add(card.face);
-     const cardsHtml = `<div class="card ${card.face}"></div>`;
-
-    // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup 
-    // const cardsHtml = deck.reduce(function(html, card) {
-    //   return html + `<div class="card ${card.face}"></div>`;
-    // }, '');
-    console.log(card, container);
+    const cardsHtml = `<div class="card ${card.face}"></div>`;
     container.innerHTML = cardsHtml;
   }
 //shuffle the deck
@@ -123,15 +145,16 @@ function buildOriginalDeck() {
     return deck;
 }
 function renderResults() {
-    const resultContainer = document.getElementById('resultContainer');
-//Display the results based on 'results' and 'winner' variables
-    if (results.p === 'win') {
+    let resultContainer = document.getElementById('resultContainer');
+//Display the results based on 'results'
+    if (results.p === 'p') {
         resultContainer.textContent = 'Player wins this round!';
-    } else if (results.c === 'win') {
+    } else if (results.c === 'c') {
         resultContainer.textContent = 'Computer wins this round!';
     } else {
         resultContainer.textContent = "It's a tie! Time to go to war!";
     }
+    
 }
 
 function render() {
@@ -151,35 +174,6 @@ function init() {
         p: '',
         c: '',
     };
-
-
-    renderDeckInContainer(playerCards, pCardEl);
-    renderDeckInContainer(computerCards, cCardEl);
-    renderResults();
+    render();
 }
-
-
 init();
-
-//Initialize the deck and shuffle it
-
-//Split the deck between two players
-
-//Play the game until one player runs out of cards
-//When there's a tie go to war! If there's a tie again. War again!
-
-
-// Determine the winner.
-
-
-
-//split the cards up 26/26 between player1/computer
-//Gameplay:
-//each player flips a card from the top of their pile.
-//check to see who has the higher card.
-//whoever has higher card wins the cards on field and it goes in the bottom of their deck.
-//if their is no higher card that means there's a tie. War begins!
-//each player places 4 more cards down. The last card on top is used to battle.
-//if their is another tie. Another war happens.
-//winner takes all the cards on the "field".
-//This process repeats until One player has all the cards or one player runs out of cards during war.
